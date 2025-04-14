0d84f8c2312fab04ef092f4e00fd6ccd15cede6a
mrBorland <taras-tiva@ukr.net>
2025-04-14 17:51:21 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+
+def start_bot():
+    print("SproutGigs бот запущено...")
+    while True:
+        print("Виконую завдання...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    start_bot()
